540bbbbbbc72a6c5f585b598bf9147fe84e26cd7
Josh Hethcox <josh.hethcox@gmail>
2018-06-07 16:39:08 -0400

    handle work

--- todo.js
+++ todo.js
@@ -1,6 +1,23 @@
-const R = require('ramda')
+const { head } = require('ramda')
 
+const [execPath, jsPath, cmd, ...rest] = process.argv
 
-const things = ["bugger", 8, null, false, "bar"]
+// console.log(`execPath`, execPath)
+// console.log(`jsPath`, execPath)
+// console.log(`cmd`, cmd)
+// console.log(`rest`, rest)
 
-console.log(R.head(things))
+function handleCmd(cmd) {
+  switch (cmd) {
+    case 'add':
+      return 'you asked for add'
+    case 'rm':
+      return 'you asked for remove'
+    case 'init':
+      return 'you asked for init'
+    default:
+      return `${cmd} is not supported`
+  }
+}
+
+console.log(handleCmd(cmd))
